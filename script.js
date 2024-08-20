1cd33aa4102e41c2c17b038cc77bbdd05c5c6e0c
mdauwal <pricelezzchap@gmail.com>
2024-08-20 15:53:02 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const diceOne = document.getElementById("img-1")
+    const diceTwo = document.getElementById("img-2")
+    const allDice = [
+        "images/dice1.png",
+        "images/dice2.png",
+        "images/dice3.png",
+        "images/dice4.png",
+        "images/dice5.png",
+        "images/dice6.png",
+    ]
+   document.addEventListener("click", ()=> {
+    const index = Math.floor(Math.random() * 6);
+    const index2 = Math.floor(Math.random() * 6);
+    console.log(index) 
+    console.log(index2) 
+
+    diceOne.src = allDice[index];
+    diceTwo.src = allDice[index2];
+   })
+
+    
+})
